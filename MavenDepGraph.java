19c98205970ae8e8ba6acd641f502a44b6150b25
Jason Lee <jason@steeplesoft.com>
2026-06-16 14:22:17 -0500

    Modify -o option to support rendering graph as image

--- MavenDepGraph.java
+++ MavenDepGraph.java
@@ -1,12 +1,12 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
 //DEPS org.aesh:aesh:3.8
 //DEPS org.apache.maven:maven-model:3.9.16
+//DEPS guru.nidi:graphviz-java:0.18.1
 //JAVA 17+
 
-import java.io.FileOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.PrintStream;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -17,6 +17,8 @@ import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
 
+import guru.nidi.graphviz.engine.Format;
+import guru.nidi.graphviz.engine.Graphviz;
 import org.aesh.AeshRuntimeRunner;
 import org.aesh.command.Command;
 import org.aesh.command.CommandDefinition;
@@ -43,6 +45,7 @@ public class MavenDepGraph implements Command<CommandInvocation> {
     @Option(shortName = 'o', aliases = {"outputFile"})
     private String outputFileName;
 
+    private Set<String> validFormats = Set.of("png", "svg");
     private Map<String, List<String>> deps = new HashMap<>();
 
     public static void main(String[] args) throws MalformedURLException {
@@ -59,7 +62,7 @@ public class MavenDepGraph implements Command<CommandInvocation> {
                 processArtifact(gavToDependency(artifact));
             }
 
-            outputDotGraph();
+            outputGraph();
 
             return CommandResult.SUCCESS;
         } catch (IOException | XmlPullParserException e) {
@@ -68,35 +71,45 @@ public class MavenDepGraph implements Command<CommandInvocation> {
         }
     }
 
-    private void outputDotGraph() throws IOException {
-        try(PrintStream out = (outputFileName != null) ? new PrintStream(new FileOutputStream(outputFileName)) : System.out) {
-            out.println("digraph MavenDepGraph {");
-            // Define nodes
-            out.println("\t{");
-//            out.println("\t\tnode [shape=component]");
-            out.println("\t\tnode [shape=\"box\",style=\"rounded\",fontname=\"Helvetica\",fontsize=\"14\"]");
-            out.println("\t\tedge [fontsize=\"10\",fontname=\"Helvetica\"]");
-            deps.keySet().forEach(entry -> {
-                out.printf("\t\t\"%s\" [label=\"%s\"]%n", entry, entry.replace(":",":\\n"));
-            });
-            out.println("\t}");
+    private void outputGraph() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("digraph MavenDepGraph {\n");
+        sb.append("\t{\n");
+        sb.append("\t\tnode [shape=\"box\",style=\"rounded\",fontname=\"Helvetica\",fontsize=\"14\"]\n");
+        sb.append("\t\tedge [fontsize=\"10\",fontname=\"Helvetica\"]\n");
+        deps.keySet().forEach(entry -> {
+            sb.append(String.format("\t\t\"%s\" [label=\"%s\"]%n", entry, entry.replace(":", ":\\n")));
+        });
+        sb.append("\t}\n");
 
-            // Output edges
-            deps.forEach((parent, children) -> {
-                out.printf("\t \"%s\" -> {%s}%n", parent,
-                        children.stream()
-                                .map(d -> "\"" + d + "\"")
-                                .collect(Collectors.joining(", ")));
-            });
-            out.println("}");
+        deps.forEach((parent, children) -> {
+            sb.append(String.format("\t \"%s\" -> {%s}%n", parent,
+                    children.stream()
+                            .map(d -> "\"" + d + "\"")
+                            .collect(Collectors.joining(", "))));
+        });
+        sb.append("}\n");
+
+        if (outputFileName != null) {
+            Graphviz.fromString(sb.toString())
+                    .render(getFormat(getFileExtension(outputFileName)))
+                    .toFile(new File(outputFileName));
+        } else {
+            System.out.println(sb);
         }
     }
 
+    private String getFileExtension(String outputFileName) {
+        int index = outputFileName.lastIndexOf(".");
+        return index == -1 ? "png" : outputFileName.substring(index + 1);
+    }
+
     private void processArtifact(Dependency parent) throws XmlPullParserException, IOException {
-        if (!deps.containsKey(parent)) {
+        String parentGav = depToGav(parent);
+        if (!deps.containsKey(parentGav)) {
             try (InputStream inputStream = openStream(new URL(getUrlForDependency(parent)))) {
                 Model model = new MavenXpp3Reader().read(inputStream);
-                List<String> localDeps = deps.computeIfAbsent(depToGav(parent), k -> new ArrayList<>());
+                List<String> localDeps = deps.computeIfAbsent(parentGav, k -> new ArrayList<>());
                 for (Dependency dep : model.getDependencies()) {
                     localDeps.add(depToGav(dep));
                     processArtifact(dep);
@@ -144,5 +157,14 @@ public class MavenDepGraph implements Command<CommandInvocation> {
             throw new IllegalArgumentException("Expected 200 response, got " + responseCode + " for " + url);
         }
         return connection.getInputStream();
+    }
+
+    private Format getFormat(String option) {
+        return switch (option) {
+            case "png" -> Format.PNG;
+            case "svg" -> Format.SVG;
+            case "dot" -> Format.DOT;
+            default -> throw new RuntimeException("Invalid format: " + option);
+        };
     }
 }
